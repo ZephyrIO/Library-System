c9ec7197d1acf3d098971f259b13ada5c887f06c
ZephyrIO <66318348+ZephyrIO@users.noreply.github.com>
2024-12-18 14:33:07 -0500

    added page labels

--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <div>
       <h1>Fictional County Library System</h1>
+      <h3>Login</h3>
       <Login />
     </div>
   )
--- src/app/register/page.js
+++ src/app/register/page.js
@@ -4,6 +4,7 @@ export default function RegisterPage() {
   return (
     <div>
       <h1>Fictional County Library System</h1>
+      <h3>Register</h3>
       <Register />
     </div>
   )
