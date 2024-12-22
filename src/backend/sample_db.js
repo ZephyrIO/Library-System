1eba4a644c94a0a86d5a97345d039fd9bafa1fad
ZephyrIO <66318348+ZephyrIO@users.noreply.github.com>
2024-12-22 17:04:10 -0500

    added instructions for database design

--- src/backend/sample_db.js
+++ src/backend/sample_db.js
@@ -9,3 +9,30 @@ const sql = postgres({
 });
 
 export default sql;
+
+/**
+ * The database should be designed as follows:
+ * Table: users
+ * ----> id : serial PRIMARY
+ * ----> email : varchar
+ * ----> password : varchar
+ * ----> isAdmin : boolean
+ * ----> lastUpd_Usr : varchar
+ * ----> lastUpd_TS : timestamp (without timezone)
+ * Table: items
+ * ----> id : serial PRIMARY
+ * ----> title : varchar
+ * ----> type : varchar
+ * ----> creator : varchar
+ * ----> release_date : timestamp (without timezone)
+ * ----> lastUpd_Usr : varchar
+ * ----> lastUpd_TS : timestamp (without timezone)
+ * Table: item_ledger
+ * ----> ledger_id : serial PRIMARY
+ * ----> item_id : integer FOREIGN
+ * ----> user_id : integer FOREIGN
+ * ----> checkout_TS : timestamp (without timezone)
+ * ----> checkin_TS : timestamp (without timezone) NULLABLE
+ * ----> lastUpd_Usr : varchar
+ * ----> lastUpd_TS : timestamp (without timezone)
+ */
