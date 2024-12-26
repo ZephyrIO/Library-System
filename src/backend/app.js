9458661725ede1ec2eddc2110a387767dc2ad242
ZephyrIO <66318348+ZephyrIO@users.noreply.github.com>
2024-12-26 18:20:01 -0500

    started work on new backend design

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,14 +1,6 @@
-import postgres from 'postgres';
+const express = require('express');
 
-const sql = postgres({
-    host: '',           // Postgres ip address[s] or domain name[s]
-    port: 5432,         // Postgres server port[s]
-    database: '',       // Name of database to connect to
-    username: '',       // Username of database user
-    password: '',       // Password of database user
-});
-
-export default sql;
+const app = express();
 
 /**
  * The database should be designed as follows:
