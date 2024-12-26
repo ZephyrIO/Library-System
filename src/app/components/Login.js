4826860192872b23348dee8e949026e6f997bbd3
ZephyrIO <66318348+ZephyrIO@users.noreply.github.com>
2024-12-26 18:05:52 -0500

    added import statement

--- src/app/components/Login.js
+++ src/app/components/Login.js
@@ -1,6 +1,6 @@
 'use client';
+import { useState } from "react";
 import { useRouter } from "next/navigation";
-import sql from '@/backend/db.js';
 import './Login.css';
 
 export default function Login() {
