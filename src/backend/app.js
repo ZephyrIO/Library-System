b4d65d11d964387334ab2545cf4e927adf0ae1b5
ZephyrIO <66318348+ZephyrIO@users.noreply.github.com>
2024-12-26 18:56:57 -0500

    working on backend

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,6 +1,11 @@
 const express = require('express');
 
 const app = express();
+const PORT = 3001;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
 
 /**
  * The database should be designed as follows:
