0e439d986ebb3bd7d13db38e7894d29cb8501091
ZephyrIO <66318348+ZephyrIO@users.noreply.github.com>
2024-12-27 18:35:33 -0500

    added password match check

--- src/app/components/Register.js
+++ src/app/components/Register.js
@@ -15,21 +15,26 @@ export default function Register() {
     async function handleRegister (e)
     {
         e.preventDefault();
-        try 
+        if (formData.password == formData.confirmPassword)
         {
-            const response = await axios.post('http://localhost:3001/api/register', formData);
-            alert(response.data.message);
-            router.push('/');
-        } catch (error)
-        {
-            console.error('Error registering:', error);
+            try 
+            {
+                const response = await axios.post('http://localhost:3001/api/register', formData);
+                alert(response.data.message);
+                router.push('/');
+            } catch (error)
+            {
+                console.error('Error registering:', error);
 
-            // Show the exact error message from the backend
-            if (error.response && error.response.data) {
-                alert(`Registration failed: ${error.response.data.message}`);
-            } else {
-                alert('Registration failed. Please try again.');
+                // Show the exact error message from the backend
+                if (error.response && error.response.data) {
+                    alert(`Registration failed: ${error.response.data.message}`);
+                } else {
+                    alert('Registration failed. Please try again.');
+                }
             }
+        } else {
+            alert('Registration failed: Passwords do not match. Try Again.');
         }
     }
 
