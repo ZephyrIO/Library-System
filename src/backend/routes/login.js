ce374777f93fc25ec0317fc403d510ae954fe2c8
ZephyrIO <66318348+ZephyrIO@users.noreply.github.com>
2025-01-01 16:40:46 -0500

    updated login message

--- src/backend/routes/login.js
+++ src/backend/routes/login.js
@@ -56,7 +56,7 @@ router.post('/login', async (req, res) => {
                 email: email,
                 isadmin: userIsAdmin,
             },
-            message: 'Account created successfully',
+            message: 'Logged in successfully',
         });
     } catch (err) {
         res.status(500).json({error: err});
